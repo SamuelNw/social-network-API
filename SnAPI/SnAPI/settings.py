42f16766852e19552a86a4548400dceda7a9af8c
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-07 01:37:11 +0300

    Initiate django-rest-framework in the project

--- SnAPI/SnAPI/settings.py
+++ SnAPI/SnAPI/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST-FRAMEWORK CONFIG
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
