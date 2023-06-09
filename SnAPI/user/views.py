97ee9cd119f932ce2ff4816ac6a39528bedc1af9
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-09 19:20:49 +0300

    Add user creation view

--- SnAPI/user/views.py
+++ SnAPI/user/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+"""
+User API Views.
+"""
 
-# Create your views here.
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class UserAPIView(generics.CreateAPIView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
