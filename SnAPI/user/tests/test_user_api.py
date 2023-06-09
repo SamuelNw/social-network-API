d5e88b614aaec0bf5092dc4998a61c2885c818bc
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-09 18:52:38 +0300

    Test short passwords not accepted

--- SnAPI/user/tests/test_user_api.py
+++ SnAPI/user/tests/test_user_api.py
@@ -53,3 +53,15 @@ class PublicUserAPITests(TestCase):
 
     def test_password_too_short(self):
         """Test passwords are longer than 5 chars."""
+        payload = {
+            "email": "user@example.com",
+            "password": "pw",
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload["email"]
+        ).exists()
+
+        self.assertFalse(user_exists)
