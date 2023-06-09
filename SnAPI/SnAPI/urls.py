2a2bc3b937371a941d53b476dbb8c6b1ed156c10
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-09 19:37:30 +0300

    Fix Auto docs error

--- SnAPI/SnAPI/urls.py
+++ SnAPI/SnAPI/urls.py
@@ -25,8 +25,6 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="restframework")),
 
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
