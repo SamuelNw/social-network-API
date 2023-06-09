ff3319cdcccfd919793dfde22c464fd7848c9a26
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-09 19:25:40 +0300

    Configure user API urls

---
+++ SnAPI/user/urls.py
@@ -0,0 +1,14 @@
+"""
+User API urls.
+"""
+
+from django.urls import path
+
+from views import CreateUserAPIView
+
+
+app_name = "user"
+
+urlpatterns = [
+    path("create/", CreateUserAPIView.as_view(), name="create")
+]
