6430287dc0cc9ab99e8b635fcfd68a5bc35a7456
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-10 13:54:11 +0300

    Create a view for auth token

--- SnAPI/user/views.py
+++ SnAPI/user/views.py
@@ -3,10 +3,21 @@ User API Views.
 """
 
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
 
 
 class CreateUserAPIView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+
+class AuthTokenAPIView(ObtainAuthToken):
+    """Generate auth tokens for users."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
