f99813f369523306e63c0e7fe6ff72862ecc7be6
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-06-10 13:57:31 +0300

    Implement Token Authentication

--- SnAPI/user/urls.py
+++ SnAPI/user/urls.py
@@ -4,11 +4,15 @@ User API urls.
 
 from django.urls import path
 
-from .views import CreateUserAPIView
+from .views import (
+    CreateUserAPIView,
+    AuthTokenAPIView
+)
 
 
 app_name = "user"
 
 urlpatterns = [
-    path("create/", CreateUserAPIView.as_view(), name="create")
+    path("create/", CreateUserAPIView.as_view(), name="create"),
+    path("token/", AuthTokenAPIView.as_view(), name="token"),
 ]
